476dfbdc1b6b4b4a1351aedda64e078d2f2adb8a
poirier-aleksandra <puarjeshura2004@gmail.com>
2024-10-27 18:30:58 +0300

    Лабораторная работа 2

---
+++ Лабораторная работа 2/task_1.py
@@ -0,0 +1,13 @@
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.03  # Ежемесячный рост цен
+
+months = 0
+
+while money_capital > spend:
+    money_capital += salary
+    money_capital -= spend
+    spend *= 1 + increase
+    months += 1
+print("Количество месяцев, которое можно протянуть без долгов:",  months)
